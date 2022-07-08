c193542de256389c37e338f5359a7d43a3e1ad27
Taoning Ge <gtn1024o@outlook.com>
2022-07-08 14:23:35 +0800

    feat(remove): 删除无用参数

--- src/services/typings.d.ts
+++ src/services/typings.d.ts
@@ -3,23 +3,12 @@
 
 declare namespace API {
   type CurrentUser = {
+    id?: number;
     username?: string;
     avatar?: string;
-    userid?: string;
-    email?: string;
-    signature?: string;
-    title?: string;
-    group?: string;
-    tags?: { key?: string; label?: string }[];
     notifyCount?: number;
     unreadCount?: number;
-    country?: string;
     access?: string;
-    geographic?: {
-      province?: { label?: string; key?: string };
-      city?: { label?: string; key?: string };
-    };
-    address?: string;
     phone?: string;
   };
 
