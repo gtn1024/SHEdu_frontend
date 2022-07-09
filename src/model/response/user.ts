b5a238ee18671f02eb0d7e25c8d7ed67cf20b071
Taoning Ge <gtn1024o@outlook.com>
2022-07-09 16:34:30 +0800

    修改CurrentUser

--- src/model/response/user.ts
+++ src/model/response/user.ts
@@ -1,9 +1,34 @@
 export type CurrentUser = {
+  /**
+   * 用户id
+   */
   id?: number;
+  /**
+   * 用户名
+   */
   username?: string;
+  /**
+   * 头像
+   */
   avatar?: string;
+  /**
+   * 通知数量
+   */
   notifyCount?: number;
+  /**
+   * 未读通知数量
+   */
   unreadCount?: number;
+  /**
+   * 用户角色
+   */
   role?: string;
+  /**
+   * 手机号
+   */
   phone?: string;
+  /**
+   * 是否是VIP
+   */
+  isVip?: boolean;
 };
